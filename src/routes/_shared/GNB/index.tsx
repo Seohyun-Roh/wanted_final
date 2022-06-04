f0a062d8b041936757a52bbceb9ea194e397a51e
Seohyun Roh <radult951@gmail.com>
2022-06-05 03:13:19 +0900

    feat: GNB

---
+++ src/routes/_shared/GNB/index.tsx
@@ -0,0 +1,24 @@
+import cx from 'classnames'
+import { NavLink } from 'react-router-dom'
+import styles from './gnb.module.scss'
+
+const GNB = () => {
+  return (
+    <nav className={styles.gnb}>
+      <ul>
+        <li>
+          <NavLink to='/' className={({ isActive }) => cx({ [styles.isActive]: isActive })}>
+            <p>MAIN</p>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/myFavList' className={({ isActive }) => cx({ [styles.isActive]: isActive })}>
+            <p>FAVORITE</p>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default GNB
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom'
 
 import styles from './routes.module.scss'
 import HomePage from './Home'
+import GNB from './_shared/GNB'
 
 const App = () => {
   return (
     <div className={styles.container}>
       <main className={styles.app}>
+        <GNB />
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/myFavList' element={<div>HH</div>} />
+          <Route path='*' element={<div>Not found</div>} />
         </Routes>
       </main>
     </div>
